edb5ecb7459ba64916668942af043303cd603570
theMungai <mungaithesimon@gmail.com>
2025-04-13 15:52:48 +0300

    List and keys

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -7,10 +7,14 @@ function ColorList() {
     "salmon",
     "darkslategray",
     "hotpink",
+    "yellow",
+    "red",
+    "white"
   ];
 
   return (
-    <div>
+    <>
+      {/* <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
         <li style={{ color: colors[0] }}>{colors[0]}</li>
@@ -19,8 +23,18 @@ function ColorList() {
         <li style={{ color: colors[3] }}>{colors[3]}</li>
         <li style={{ color: colors[4] }}>{colors[4]}</li>
       </ol>
+    </div> */}
+
+    <div>
+      <h1>Top 5 CSS Colors</h1>
+      <ol>
+      {colors.map((color) => (
+        <li key={color} style={{color:color}}>{color}</li>
+      ))}
+      </ol>
     </div>
+    </>
   );
 }
-
+// 
 export default ColorList;
